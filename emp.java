abc1e07b9d8116160a319e985b9478a6c3bdd4b2
aditibp16 <aditibpatil9@gmail.com>
2025-04-22 14:39:45 +0530

    Save the place

---
+++ emp.java
@@ -0,0 +1,14 @@
+public class emp {
+    public static void main(String[] args) {
+        String name = "xyz";
+        int id = 56;
+        int salary = 20000;
+        String company = "abc";
+
+        System.out.println("----employeee detailss----");
+        System.out.println("name"+name);
+        System.out.println("id"+id);
+        System.out.println("salary"+salary);
+        System.out.println("company"+company);
+    }
+}
