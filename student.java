408de873439a50acee9767655d5f0b4bb3b28cc2
aditibp16 <aditibpatil9@gmail.com>
2025-04-22 15:30:22 +0530

    save the place

---
+++ student.java
@@ -0,0 +1,14 @@
+public class student {
+    public static void main(String[] args) {
+        String name = "Aditi";
+        int id = 66;
+        String course ="BCA";
+        String mark = "Pass";
+
+        System.out.println("----Student detailss----");
+        System.out.println("name: "+name);
+        System.out.println("id: "+id);
+        System.out.println("salary: "+course);
+        System.out.println("company: "+mark);
+}
+}
