7a6413307ab156cf04eebf7b7139aaeb5b443f53
aditibp16 <aditibpatil9@gmail.com>
2025-04-22 15:53:00 +0530

    Save

---
+++ course.java
@@ -0,0 +1,10 @@
+public class course {
+    public static void main(String[] args) {
+        String name = "BCA";
+        int course_id = 66;
+
+        System.out.println("----Course detailss----");
+        System.out.println("name: "+name);
+        System.out.println("course_id: "+course_id);
+    }
+}
